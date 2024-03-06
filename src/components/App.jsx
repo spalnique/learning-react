9495404dabe1eb3c22b9b38d4016969cd62f2e17
spalnique <lex.sydoruk@gmail.com>
2024-03-06 20:37:43 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,21 @@
 import './App.css';
-import Product from './Product';
+import { Product } from './Product';
 
 export default function App() {
   return (
     <div>
       <h1>Best selling</h1>
-      <Product />
-      <Product />
-      <Product />
+
+      <Product
+        name="Tacos With Lime"
+        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+        price={10.99}
+      />
+      <Product
+        name="Fries and Burger"
+        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+        price={14.29}
+      />
     </div>
   );
 }
